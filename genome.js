8ca3023bcd6d1e6b68cb326038bf05b563aafc1f
GRMWeb <gmacarioweb@gmail.com>
2018-04-02 20:22:20 -0700

    First attempt at genome.js Exercise

---
+++ genome.js
@@ -0,0 +1,48 @@
+function solution(S, P, Q) {
+    var prefA = [0];
+    var prefC = [0];
+    var prefG = [0];
+    var prefT = [0];
+    var impacts = [];
+
+    for (var i = 0; i < S.length; i++) {
+        prefA.push(prefA[i]);
+        prefC.push(prefC[i]);
+        prefG.push(prefG[i]);
+        prefT.push(prefT[i]);
+
+        if (S[i] === 'A') {
+            prefA[i + 1] += 1;
+        } else if (S[i] === 'C') {
+            prefC[i+1] += 1;
+        } else if (S[i] === 'G') {
+            prefG[i+1] += 1;
+        } else if (S[i] === 'T') {
+            prefT[i+1] += 1;
+        }
+    }
+
+    for (var i = 0; i < P.length; i++) {
+        if(prefA[Q[i] +1] > prefA[P[i] + 1]) {
+            impacts.push(1);
+        } else if (prefC[Q[i] + 1] > prefC[P[i] + 1]) {
+            impacts.push(2);
+        } else if (prefG[Q[i] + 1] > prefG[P[i] + 1]) {
+            impacts.push(3);
+        } else if (prefT[Q[i] + 1] > prefT[P[i] + 1]) {
+            impacts.push(4);
+        } else if (P[i] === Q[i]) {
+            if (S[P[i]] === 'A') {
+                impacts.push(1);
+            } else if (S[P[i]] === 'C') {
+                impacts.push(2);
+            } else if (S[P[i]] === 'G') {
+                impacts.push(3);
+            } else if (S[P[i]] === 'T') {
+                impacts.push(4);
+            }
+        }
+    }
+
+    return impacts;
+}
